c33e19b3e8030476879dbe2ebd54b067dd633b15
grum-tez <grum.tez@gmail.com>
2022-11-24 14:54:38 +1300

    second attempt at address validator following feedback

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { BigNumber } from 'bignumber.js'
-
+import bs58check = require("bs58check")
+import { Buffer } from 'buffer'
 /* Michleline -------------------------------------------------------------- */
 
 export type Mprim = {
@@ -168,9 +169,11 @@ export const getter_args_to_mich = (arg: Micheline, callback: Entrypoint): Miche
 
 export class Address implements ArchetypeType {
   private _content: string
+  
+
   constructor(v: string) {
-    this._content = this.validate_address(v)
-    }
+    this._content = this.is_address_valid(v)
+  }
   to_mich(): Micheline {
     return string_to_mich(this._content)
   }
@@ -180,20 +183,33 @@ export class Address implements ArchetypeType {
   toString(): string {
     return this._content
   }
+ 
+  private decode_lengths : {[char: string] : number} = {
+    tz1: 20,
+    tz2: 20,
+    tz3: 20, 
+    tz4: 20,
+    txr1: 20,
+    KT1: 20,
+    KT: 20,
+  }
+ 
+  private is_address_valid(input: string){
+    const prefixes = Object.keys(this.decode_lengths)
 
-    private validate_address(input: string) {
-      let valid_address = false
-      const regexp = new RegExp(/(tz|KT)[0-9][A-Za-z0-9]{33}/)
-      const address_match = input.match(regexp)
-      if (address_match !== null) 
-        valid_address = address_match[0] === input 
-      if (valid_address) {
-        return input
-      } else { 
-        throw new Error(`Invalid address input. Recieved input: ${input}`)
-      }
-    }
+    const match = new RegExp(`^(${prefixes.join('|')})`).exec(input);
+    if (!match || match.length === 0) throw new Error(`No matching prefix found. Recieved input: ${input}`)
+    
+    const prefix = match[0];
 
+    const decodedLong = bs58check.decodeUnsafe(input);
+    if (!decodedLong)  throw new Error(`Address is not b58 encoding compatible. Recieved input: ${input}`)
+    
+    const decoded = new Uint8Array(decodedLong).slice(prefix.length)
+    
+    if (decoded.length !== this.decode_lengths[prefix]) throw new Error(`The decoded Address output is the wrong length for the given prefix of ${prefix}.`)
+    return input
+  }
 }
 
 export class Bls12_381_fr implements ArchetypeType {
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,204 +1,210 @@
 import BigNumber from 'bignumber.js';
 import { Address, Micheline, mich_to_ticket, Mstring, Nat, Rational, Ticket, Duration } from '../src/main'
 
-describe('ArchetypeType', () => {
-  // describe('Address', () => {
-  // });
+// describe('ArchetypeType', () => {
+//   // describe('Address', () => {
+//   // });
 
-  describe('Nat', () => {
-    describe('Constructor', () => {
-      test('Fails if neg number', () => {
-        expect(() => { new Nat(-5) }).toThrow("Not an Nat value: -5")
-      });
-    });
+//   describe('Nat', () => {
+//     describe('Constructor', () => {
+//       test('Fails if neg number', () => {
+//         expect(() => { new Nat(-5) }).toThrow("Not an Nat value: -5")
+//       });
+//     });
 
-    describe('toString', () => {
+//     describe('toString', () => {
 
-      test('Number simple', () => {
-        expect(new Nat(5).toString()).toBe("5");
-      });
+//       test('Number simple', () => {
+//         expect(new Nat(5).toString()).toBe("5");
+//       });
 
-      test('String simple', () => {
-        expect(new Nat("5").toString()).toBe("5");
-      });
+//       test('String simple', () => {
+//         expect(new Nat("5").toString()).toBe("5");
+//       });
 
-      test('Bignumber simple', () => {
-        expect(new Nat(new BigNumber("5")).toString()).toBe("5");
-      });
+//       test('Bignumber simple', () => {
+//         expect(new Nat(new BigNumber("5")).toString()).toBe("5");
+//       });
 
-      test('String big', () => {
-        expect(new Nat("9999999999999999999999999999999999999").toString()).toBe("9999999999999999999999999999999999999");
-      });
+//       test('String big', () => {
+//         expect(new Nat("9999999999999999999999999999999999999").toString()).toBe("9999999999999999999999999999999999999");
+//       });
 
-      test('Bignumber big', () => {
-        expect(new Nat(new BigNumber("9999999999999999999999999999999999999")).toString()).toBe("9999999999999999999999999999999999999");
-      });
-    })
-  })
+//       test('Bignumber big', () => {
+//         expect(new Nat(new BigNumber("9999999999999999999999999999999999999")).toString()).toBe("9999999999999999999999999999999999999");
+//       });
+//     })
+//   })
 
-  describe('Rational', () => {
-    describe('toString', () => {
-      it('String simple', () => {
-        expect(new Rational("5").toString()).toBe("5");
-      });
+//   describe('Rational', () => {
+//     describe('toString', () => {
+//       it('String simple', () => {
+//         expect(new Rational("5").toString()).toBe("5");
+//       });
 
-      it('Number simple', () => {
-        expect(new Rational(5).toString()).toBe("5");
-      });
+//       it('Number simple', () => {
+//         expect(new Rational(5).toString()).toBe("5");
+//       });
 
-      it('Number decimal', () => {
-        expect(new Rational(5.4464).toString()).toBe("5.4464");
-      });
+//       it('Number decimal', () => {
+//         expect(new Rational(5.4464).toString()).toBe("5.4464");
+//       });
 
-      it('String decimal', () => {
-        expect(new Rational("5.4464").toString()).toBe("5.4464");
-      });
+//       it('String decimal', () => {
+//         expect(new Rational("5.4464").toString()).toBe("5.4464");
+//       });
 
-      it('String decimal percent', () => {
-        expect(new Rational("5.4464%").toString()).toBe("0.054464");
-      });
+//       it('String decimal percent', () => {
+//         expect(new Rational("5.4464%").toString()).toBe("0.054464");
+//       });
 
-      it('String with big number', () => {
-        expect(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).toString()).toBe("100000000000.00435435435425664606");
-      });
+//       it('String with big number', () => {
+//         expect(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).toString()).toBe("100000000000.00435435435425664606");
+//       });
 
-    });
+//     });
 
-    describe('to_number', () => {
-      it('String simple', () => {
-        expect(new Rational("5").to_number()).toBe(5);
-      });
+//     describe('to_number', () => {
+//       it('String simple', () => {
+//         expect(new Rational("5").to_number()).toBe(5);
+//       });
 
-      it('Number simple', () => {
-        expect(new Rational(5).to_number()).toBe(5);
-      });
+//       it('Number simple', () => {
+//         expect(new Rational(5).to_number()).toBe(5);
+//       });
 
-      it('Number decimal', () => {
-        expect(new Rational(5.4464).to_number()).toBe(5.4464);
-      });
+//       it('Number decimal', () => {
+//         expect(new Rational(5.4464).to_number()).toBe(5.4464);
+//       });
 
-      it('String decimal', () => {
-        expect(new Rational("5.4464").to_number()).toBe(5.4464);
-      });
+//       it('String decimal', () => {
+//         expect(new Rational("5.4464").to_number()).toBe(5.4464);
+//       });
 
-      it('String decimal percent', () => {
-        expect(new Rational("5.4464%").to_number()).toBe(0.054464);
-      });
+//       it('String decimal percent', () => {
+//         expect(new Rational("5.4464%").to_number()).toBe(0.054464);
+//       });
 
-      it('String with big number', () => {
-        expect(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).to_number()).toBe(100000000000.00435);
-      });
-    })
+//       it('String with big number', () => {
+//         expect(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).to_number()).toBe(100000000000.00435);
+//       });
+//     })
 
-    describe('Ticket', () => {
-      it('Ticket', () => {
-        const tjson: Micheline = {
-          "prim": "Pair",
-          "args": [
-            {
-              "string": "KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"
-            },
-            {
-              "string": "info"
-            },
-            {
-              "int": "1"
-            }
-          ]
-        };
+//     describe('Ticket', () => {
+//       it('Ticket', () => {
+//         const tjson: Micheline = {
+//           "prim": "Pair",
+//           "args": [
+//             {
+//               "string": "KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"
+//             },
+//             {
+//               "string": "info"
+//             },
+//             {
+//               "int": "1"
+//             }
+//           ]
+//         };
 
-        const ticket_actual = mich_to_ticket<string>(tjson, (x: Micheline): string => { return (x as Mstring).string });
-        expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("info" as string), new Nat(1)).equals(ticket_actual)).toBe(true)
-        expect(new Ticket(new Address("KT1XcpRnLQANuGCJ9SZW3GXVG8BArUKymqtk"), ("info" as string), new Nat(1)).equals(ticket_actual)).toBe(false)
-        expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("infu" as string), new Nat(1)).equals(ticket_actual)).toBe(false)
-        expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("info" as string), new Nat(2)).equals(ticket_actual)).toBe(false)
-      })
-    })
+//         const ticket_actual = mich_to_ticket<string>(tjson, (x: Micheline): string => { return (x as Mstring).string });
+//         expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("info" as string), new Nat(1)).equals(ticket_actual)).toBe(true)
+//         expect(new Ticket(new Address("KT1XcpRnLQANuGCJ9SZW3GXVG8BArUKymqtk"), ("info" as string), new Nat(1)).equals(ticket_actual)).toBe(false)
+//         expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("infu" as string), new Nat(1)).equals(ticket_actual)).toBe(false)
+//         expect(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), ("info" as string), new Nat(2)).equals(ticket_actual)).toBe(false)
+//       })
+//     })
 
-    describe('to_mich', () => {
-      it('String simple', () => {
-        expect(JSON.stringify(new Rational("5").to_mich())).toBe('{"prim":"Pair","args":[{"int":"5"},{"int":"1"}]}');
-      });
+//     describe('to_mich', () => {
+//       it('String simple', () => {
+//         expect(JSON.stringify(new Rational("5").to_mich())).toBe('{"prim":"Pair","args":[{"int":"5"},{"int":"1"}]}');
+//       });
 
-      it('Number simple', () => {
-        expect(JSON.stringify(new Rational(5).to_mich())).toBe('{"prim":"Pair","args":[{"int":"5"},{"int":"1"}]}');
-      });
+//       it('Number simple', () => {
+//         expect(JSON.stringify(new Rational(5).to_mich())).toBe('{"prim":"Pair","args":[{"int":"5"},{"int":"1"}]}');
+//       });
 
-      it('Number decimal', () => {
-        expect(JSON.stringify(new Rational(5.4464).to_mich())).toBe('{"prim":"Pair","args":[{"int":"3404"},{"int":"625"}]}');
-      });
+//       it('Number decimal', () => {
+//         expect(JSON.stringify(new Rational(5.4464).to_mich())).toBe('{"prim":"Pair","args":[{"int":"3404"},{"int":"625"}]}');
+//       });
 
-      it('String decimal', () => {
-        expect(JSON.stringify(new Rational("5.4464").to_mich())).toBe('{"prim":"Pair","args":[{"int":"3404"},{"int":"625"}]}')
-      });
+//       it('String decimal', () => {
+//         expect(JSON.stringify(new Rational("5.4464").to_mich())).toBe('{"prim":"Pair","args":[{"int":"3404"},{"int":"625"}]}')
+//       });
 
-      it('String decimal percent', () => {
-        expect(JSON.stringify(new Rational("5.4464%").to_mich())).toBe('{"prim":"Pair","args":[{"int":"851"},{"int":"15625"}]}')
-      });
+//       it('String decimal percent', () => {
+//         expect(JSON.stringify(new Rational("5.4464%").to_mich())).toBe('{"prim":"Pair","args":[{"int":"851"},{"int":"15625"}]}')
+//       });
 
-      it('String with big number', () => {
-        expect(JSON.stringify(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).to_mich())).toBe('{"prim":"Pair","args":[{"int":"5000000000000217717717712832303"},{"int":"50000000000000000000"}]}')
-      });
+//       it('String with big number', () => {
+//         expect(JSON.stringify(new Rational("99999999999999999999999956456456456999999999", new BigNumber("999999999999956456456456999999999")).to_mich())).toBe('{"prim":"Pair","args":[{"int":"5000000000000217717717712832303"},{"int":"50000000000000000000"}]}')
+//       });
 
-      it('Ticket', () => {
-        const f = (x: string): Mstring => { return { "string": x } };
-        expect(JSON.stringify(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), "info", new Nat(1)).to_mich(f))).toBe('{"prim":"Pair","args":[{"string":"KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"},{"string":"info"},{"int":"1"}]}')
-      });
-    });
+//       it('Ticket', () => {
+//         const f = (x: string): Mstring => { return { "string": x } };
+//         expect(JSON.stringify(new Ticket(new Address("KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"), "info", new Nat(1)).to_mich(f))).toBe('{"prim":"Pair","args":[{"string":"KT1PkBvorKLwdrP3UWUMo3ytZrRUq3wqfFGe"},{"string":"info"},{"int":"1"}]}')
+//       });
+//     });
 
-  })
+//   })
 
-  describe('Duration', () => {
+//   describe('Duration', () => {
 
-    test('Fails with empty string', () => {
-      expect(() => { new Duration("") }).toThrow("Invalid duration input. Received input: `' Try this format: '_w_d_h_m_s'.")
-    });
+//     test('Fails with empty string', () => {
+//       expect(() => { new Duration("") }).toThrow("Invalid duration input. Received input: `' Try this format: '_w_d_h_m_s'.")
+//     });
 
-    test('Fails with dummy string', () => {
-      expect(() => { new Duration("dummy") }).toThrow("Invalid duration input. Received input: `dummy' Try this format: '_w_d_h_m_s'.")
-    });
+//     test('Fails with dummy string', () => {
+//       expect(() => { new Duration("dummy") }).toThrow("Invalid duration input. Received input: `dummy' Try this format: '_w_d_h_m_s'.")
+//     });
 
-    test('Fails with number string', () => {
-      expect(() => { new Duration("0") }).toThrow("Invalid duration input. Received input: `0' Try this format: '_w_d_h_m_s'.")
-    });
+//     test('Fails with number string', () => {
+//       expect(() => { new Duration("0") }).toThrow("Invalid duration input. Received input: `0' Try this format: '_w_d_h_m_s'.")
+//     });
 
-    it('Simple test', () => {
-      expect(new Duration("0s").toSecond()).toBe(0)
-    })
+//     it('Simple test', () => {
+//       expect(new Duration("0s").toSecond()).toBe(0)
+//     })
 
-    it('1 second test', () => {
-      expect(new Duration("1s").toSecond()).toBe(1)
-    })
+//     it('1 second test', () => {
+//       expect(new Duration("1s").toSecond()).toBe(1)
+//     })
 
-    it('1 minute test', () => {
-      expect(new Duration("1m").toSecond()).toBe(60)
-    })
+//     it('1 minute test', () => {
+//       expect(new Duration("1m").toSecond()).toBe(60)
+//     })
 
-    it('1 hour test', () => {
-      expect(new Duration("1h").toSecond()).toBe(3600)
-    })
+//     it('1 hour test', () => {
+//       expect(new Duration("1h").toSecond()).toBe(3600)
+//     })
 
-    it('1 day test', () => {
-      expect(new Duration("1d").toSecond()).toBe(86400)
-    })
+//     it('1 day test', () => {
+//       expect(new Duration("1d").toSecond()).toBe(86400)
+//     })
 
-    it('1 week test', () => {
-      expect(new Duration("1w").toSecond()).toBe(604800)
-    })
+//     it('1 week test', () => {
+//       expect(new Duration("1w").toSecond()).toBe(604800)
+//     })
 
-    it('1 week, 1 day, 1 hour, 1 minute, 1 second test', () => {
-      expect(new Duration("1w1d1h1m1s").toSecond()).toBe(694861)
-    })
+//     it('1 week, 1 day, 1 hour, 1 minute, 1 second test', () => {
+//       expect(new Duration("1w1d1h1m1s").toSecond()).toBe(694861)
+//     })
 
-    it('3 weeks, 8 days, 4 hours, 34 minutes, 18 seconds test', () => {
-      expect(new Duration("3w8d4h34m18s").toSecond()).toBe(2522058)
-    })
+//     it('3 weeks, 8 days, 4 hours, 34 minutes, 18 seconds test', () => {
+//       expect(new Duration("3w8d4h34m18s").toSecond()).toBe(2522058)
+//     })
 
-  })
-})
+//   })
+// })
+
 
 
 describe('Address', () => {
+
+  test('Succeeds with valid address', () => {
+    expect(new Address("tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb").toString()).toBe("tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb")
+  })
+
 
   test('Succeeds with Valid tz1 User Address', () => {
     const input = "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
@@ -206,54 +212,35 @@ describe('Address', () => {
   })
 
   test('Succeeds with Valid tz2 User Address', () => {
-    const input = "tz2VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
+    const input = "tz28US7zJ7rLdWke75XEM3T5cLWCCxjnP4zf"
     expect(new Address(input).toString()).toBe(input)
   })
 
   test('Succeeds with Valid KT1 Contract Address', () => {
-    const input = "KT1AaaBSo5AE6Eo9fpEN5xhCD4w3kHStafxk"
-    expect(new Address(input).toString()).toBe(input)
-  })
-
-  test('Succeeds with Valid KT2 Contract Address', () => {
-    const input = "KT2AaaBSo5AE6Eo9fpEN5xhCD4w3kHStafxk"
+    const input = "KT1AaaBSo5AE6Eo8fpEN5xhCD4w3kHStafxk"
     expect(new Address(input).toString()).toBe(input)
   })
 
 
-  test('Fails with invalid character', () => {
-    const input = "tz1VSUr8ww!hLAzempoch5d6hLRiTh8Cjcjb"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
+  test('Fails with missing character', () => {
+    const input = "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcj"
+    expect(() => { new Address(input) }).toThrow(`Address is not b58 encoding compatible. Recieved input: ${input}`)
   })
 
-  test('Fails if address too long', () => {
-    const input = "tz1VSUr8wwzhLAzempoch5d6hLRiTh8Cjcjbsaf"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
-  })
-
-  test('Fails if address too short', () => {
-    const input = "KT1VSUr8wwzhLAzemp"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
-  })
 
   test('Fails with empty string', () => {
     const input = ""
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
+    expect(() => { new Address(input) }).toThrow(`No matching prefix found. Recieved input: ${input}`)
   })
 
   test('Fails with dummy string', () => {
     const input = "dummy"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
+    expect(() => { new Address(input) }).toThrow(`No matching prefix found. Recieved input: ${input}`)
   })
 
   test('Fails without contract type prefix', () => {
     const input = "VSUr8wwzhLAzempoch5d6hLRiTh8Cjcjbsaf"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
-  })
-
-  test('Fails with prefix only', () => {
-    const input = "tz1"
-    expect(() => { new Address(input) }).toThrow(`Invalid address input. Recieved input: ${input}`)
+    expect(() => { new Address(input) }).toThrow(`No matching prefix found. Recieved input: ${input}`)
   })
 
 })
